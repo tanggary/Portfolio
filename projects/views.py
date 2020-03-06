89d496b1a585aebb0e2948b195c708251472b1ee
Gary Tang <tanggary@hotmail.com>
2020-03-05 23:43:02 -0600

    80% way there!

---
+++ projects/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from projects.models import Project
+
+# Create your views here.
+def project_list(request):
+    return render(request, 'projects/index.html')
+    # return HttpResponse("<h1>Hello Raps Fan!</h1>")
+    # return none if nothing returned!
+    # Can input html strings to display for the view!
+
+def all_projects(request):
+    # query database to return all projects objects
+    projects = Project.objects.all()
+    return render(request, 'projects/all_projects.html', {'projects': projects})
