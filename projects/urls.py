37387f405ea75f45984a0da5f54617f6331b082c
Gary Tang <tanggary@hotmail.com>
2020-03-12 00:37:42 -0500

    End of Phase1!

--- projects/urls.py
+++ projects/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from projects import views
 
+app_name = 'projects'
+
 urlpatterns = [
-    path('', views.all_projects),
+    path('', views.all_projects, name='all_projects'),
+    path('<int:pk>', views.project_detail, name='project_detail'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -12,3 +12,7 @@ def all_projects(request):
     # query database to return all projects objects
     projects = Project.objects.all()
     return render(request, 'projects/all_projects.html', {'projects': projects})
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    return render(request, 'projects/detail.html', {'project': project})
